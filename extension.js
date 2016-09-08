8506096502d3132939a7fce4c6eb233f27091f0a
Jake Wilson <jake.e.wilson@gmail.com>
2016-09-08 00:16:48 -0600

    Added initial search functionality that returns results into QuickPickBox

--- extension.js
+++ extension.js
@@ -1,12 +1,51 @@
 var vscode = require('vscode');
+var request = require('request');
 
 function activate(context) {
+
+    // cdnjs.com api endpoint
+    var url = 'https://api.cdnjs.com/libraries';
 
     var disposable = vscode.commands.registerCommand('cdnjs.cdnjs', function() {
 
         var window = vscode.window;
 
-        console.log('test');
+        // Show search box
+        window.showInputBox({
+            'placeHolder': 'Search for a library, for example: jquery'
+        }).then(function(value) {
+
+            // Search the cdnjs api
+            request(url + '?search=' + value, function(err, res, body) {
+
+                // TODO: Need to add error handling here
+                // for err, res.status != 200 and !body.results
+
+                var results = JSON.parse(body).results;
+                var pickItems = [];
+
+                for (var index in results) {
+                    if (results.hasOwnProperty(index)) {
+                        var element = results[index];
+                        if (element.name) {
+                            pickItems.push(element.name);
+                        }
+                    }
+                }
+
+                window.showQuickPick(pickItems, {
+                        'placeHolder': "Results found: " + results.length
+                    })
+                    .then(function(value) {
+                        console.log(value);
+                    });
+
+
+            });
+        });
+
+
+
 
     });
 
