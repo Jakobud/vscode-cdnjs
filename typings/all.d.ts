46a6b56cccab1c1361f42e2ea6d537b2cceaa10f
Jake Wilson <jake.e.wilson@gmail.com>
2016-09-11 10:22:39 -0600

    Added Open in Browser functionality

--- typings/all.d.ts
+++ typings/all.d.ts
@@ -1,3 +1,4 @@
 /// <reference path="node.d.ts" />
-/// <reference path="globals/copy-paste/index.d.ts" />
 /// <reference path="modules/request/index.d.ts" />
+/// <reference path="globals/copy-paste/index.d.ts" />
+/// <reference path="globals/open/index.d.ts" />
