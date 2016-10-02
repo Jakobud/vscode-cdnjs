64f28ff68f65dc5c34a78b4992490915d82d7177
Jake Wilson <jake.e.wilson@gmail.com>
2016-10-01 22:27:16 -0600

    Removed unnecessary Typescript files
    Signed-off-by: Jake Wilson <jake.wilson@gmail.com>

--- typings/all.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="node.d.ts" />
-/// <reference path="modules/request/index.d.ts" />
-/// <reference path="globals/copy-paste/index.d.ts" />
-/// <reference path="globals/open/index.d.ts" />
--- typings/index.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference path="globals/copy-paste/index.d.ts" />
-/// <reference path="globals/open/index.d.ts" />
-/// <reference path="modules/request/index.d.ts" />
