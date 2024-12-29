517808da5b44ff8c8f8602e6255186320f7c6119
Jake Wilson <jake.e.wilson@gmail.com>
2024-12-28 19:59:38 -0600

    Converted show search input module to TS

--- src/lib/showSearchInput.ts
+++ src/lib/showSearchInput.ts
@@ -1,21 +1,22 @@
 'use strict';
 
-import vscode from 'vscode';
+import * as vscode from 'vscode';
 
 import settings from '../settings';
 
 // Display search box and return input
 export default async () => {
+  // Show a search input box and get the result
   let term = await vscode.window.showInputBox({
     placeHolder: `Example: ${settings.searchPlaceholders[Math.floor(Math.random() * settings.searchPlaceholders.length)]}`,
     prompt: 'Search for a script or library'
   });
 
-  term = term.trim();
-
+  // If no search term is provided, return false
   if (typeof term === 'undefined' || term === '') {
     return false;
   }
 
-  return term;
+  // Trim the search term and return it
+  return term.trim();
 };
